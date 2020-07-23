d36db06b30f064ea8072aa4781ba53d9b1f164ad
lindsayturner <lindsay.c.turner1@gmail.com>
2020-07-23 09:51:17 -0700

    First commit

---
+++ raster_tests.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 13:35:55 2020
+
+@author: Lindsay Turner
+"""
+##############################################################################
+# IMPORT
+##############################################################################
+
+import rasterio
+from rasterio.mask import mask
+from rasterio.plot import show
+
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import mapping
+import matplotlib.pyplot as plt
+
+import os
+
+##############################################################################
+# RASTER
+##############################################################################
+
+# Read in data:
+raw_fp = "C:/Users/linds/NOAA/shapefile_training/data_raw/" # file path to raw data folder
+raster_fp = raw_fp + 'white_small_sub.tif' # combine with tif file
+raster = rasterio.open(raster_fp)
+type(raster)
+raster.meta # This gives information on the raster file
+
+# Read in single bands
+red = raster.read(5)
+green = raster.read(3)
+blue = raster.read(2)
+NIR2 = raster.read(8)
+
+# individual plots of bands
+show((raster, 5), cmap='Reds')
+show((raster, 3), cmap='Greens')
+show((raster, 2), cmap='Blues')
+
+# Function to normalize the grid values
+def normalize(array):
+    """Normalizes numpy arrays into scale 0.0 - 1.0"""
+    array_min, array_max = array.min(), array.max()
+    return ((array - array_min)/(array_max - array_min))
+
+# Normalize the bands
+redn = normalize(red)
+greenn = normalize(green)
+bluen = normalize(blue)
+
+# Create RGB natural color composite
+rgb = np.dstack((redn, greenn, bluen))
+
+# Plot RGB
+plt.imshow(rgb)
+
+
+# Create processed data folder: 
+#img_dir = 'C:/Users/linds/NOAA/shapefile_test/data_processed/'
+#if not os.path.exists(img_dir):
+ #   os.makedirs(img_dir)
+
+#img_fp = img_dir + 'sentinel_bands.tif'
+
+
+##############################################################################
+# SHAPEFILE
+##############################################################################
+
+shapefile_fp = raw_fp + 'Lindsay_white_river_land_cover/Lindsay_white_river_land_cover.shp'
+shapefile = gpd.read_file(shapefile_fp)
+shapefile.crs
+shapefile.bounds
